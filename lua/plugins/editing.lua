5c076cfbe42cfb37ca202cd187ec83104b7311df
Matthias Georgi <matti.georgi@gmail.com>
2023-02-07 21:29:47 +0100

    add ncim test

--- lua/plugins/editing.lua
+++ lua/plugins/editing.lua
@@ -1,5 +1,14 @@
 return {
-  "vim-test/vim-test",
+  {
+    "vim-test/vim-test",
+    keys = {
+      { "<leader>tt", ":TestNearest<CR>" },
+      { "<leader>tf", ":TestFile<CR>" },
+      { "<leader>ts", ":TestSuite<CR>" },
+      { "<leader>tl", ":TestLast<CR>" },
+      { "<leader>tv", ":TestVisit<CR>" },
+    },
+  },
   {
     "ThePrimeagen/refactoring.nvim",
     dependencies = {
